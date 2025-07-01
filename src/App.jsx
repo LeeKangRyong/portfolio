9a910244bdeccfff8a865d8c012d7a98afcb6e26
LeeKangRyong <gaiogo2@naver.com>
2025-07-01 19:19:29 +0900

    AboutMe page 따로 빼고, pages + components 구조로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { Link } from 'react-scroll';
 import { useState, useEffect, useRef } from 'react';
-import face from './assets/face.jpg';
-import gamja from './assets/gamza_gif.gif';
+import AboutMe from './pages/AboutMe';
+
 /* 1. section 별 내용 넣기 */
 /* 2. motion library로 스크롤 시 애니메이션 구현하기 */
 /* 3. article 만들기 => grid로 구현 */
@@ -49,32 +49,8 @@ function App() {
       </header>
       
       <main>
-        <section id='A'>
-          <h1>About me</h1>
-          <div className="aboutMe">
-            <div className="aboutMeTextWrapper">
-              <div className="aboutMeText">
-                <p>서비스를 만드는 프론트엔드 개발자, 이강룡 입니다. (2002.02.03, 24살)</p>
-                <p>단순 개발이 아닌, 서비스 개발 전체 흐름에 맞추어 개발하는 것을 좋아합니다.</p>
-                <p>다양한 사람들과 소통하며 사용자에게 가장 가까이 다가가 서비스를 제공한다는 점에서</p>
-                <p>프론트엔드를 깊이 파고들기로 결정하였습니다.</p>
-                <p>서비스의 도메인, 사용자 등을 고려하여 어울리는 기술스택을 탐구하고 사용하는 것을 좋아합니다.</p>
-              </div>
-            </div>
-            <img className="face" src={face} alt="증명사진" />
-          </div>
-          <div className="contactAndGamja">
-            <div className="contact">
-              <span className='contactText'>Contact) gaiogo2@naver.com</span>
-              <span className='contactText'>Github)  gaiogo2@naver.com</span>
-              <span className='contactText'>Notion)  gaiogo2@naver.com</span>
-            </div>
-            <div className="gamjaWrapper">
-              <img className="gamja" src={gamja} alt="감자" />
-              <p className='gamjaText'>집에서 키우는 거북이, 감자입니다</p>
-            </div>
-          </div>
-        </section>
+        <AboutMe />
+        
         <section id='B'>
           <h1>History</h1>
           <h2>숭실대학교 IT대학 AI융합학부 재학 중 (2020.03 ~ 현재)</h2>
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,35 @@
+import face from '../assets/face.jpg';
+import gamja from '../assets/gamza_gif.gif';
+
+function AboutMe() {
+    return (
+    <section id='A'>
+        <h1>About me</h1>
+        <div className="aboutMe">
+            <div className="aboutMeTextWrapper">
+                <div className="aboutMeText">
+                    <p>서비스를 만드는 프론트엔드 개발자, 이강룡 입니다. (2002.02.03, 24살)</p>
+                    <p>단순 개발이 아닌, 서비스 개발 전체 흐름에 맞추어 개발하는 것을 좋아합니다.</p>
+                    <p>다양한 사람들과 소통하며 사용자에게 가장 가까이 다가가 서비스를 제공한다는 점에서</p>
+                    <p>프론트엔드를 깊이 파고들기로 결정하였습니다.</p>
+                    <p>서비스의 도메인, 사용자 등을 고려하여 어울리는 기술스택을 탐구하고 사용하는 것을 좋아합니다.</p>
+                </div>
+            </div>
+                <img className="face" src={face} alt="증명사진" />
+        </div>
+        <div className="contactAndGamja">
+            <div className="contact">
+                <span className='contactText'>Contact) gaiogo2@naver.com</span>
+                <span className='contactText'>Github)  https://github.com/LeeKangRyong</span>
+                <span className='contactText'>Notion)  gaiogo2@naver.com</span>
+            </div>
+            <div className="gamjaWrapper">
+                <img className="gamja" src={gamja} alt="감자" />
+                <p className='gamjaText'>집에서 키우는 거북이, 감자입니다</p>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default AboutMe;
