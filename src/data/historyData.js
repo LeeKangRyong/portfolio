2ac5d80523a4a3bb36ab33692f5d007ccc435290
LeeKangRyong <gaiogo2@naver.com>
2025-07-04 18:52:52 +0900

    history 데이터화

---
+++ src/data/historyData.js
@@ -0,0 +1,32 @@
+const histories = [
+    {
+        title: "숭실대학교",
+        duration: "2020.03 ~ 현재",
+        description: "숭실대학교 IT대학 AI융합학부 (20, 4학년)",
+        direction: 'left',
+        delay: 0
+    },
+    {
+        title: "LG AIMERS 4기",
+        duration: "2024.01 ~ 2024.02", 
+        description: "ML 교육 및 MQL 고객 기반 B2B 영업 예측 해커톤 수료\n데이터 EDA 및 전처리 담당",
+        direction: 'right',
+        delay: 0.2
+    },
+    {
+        title: "프로메테우스 6기",
+        duration: "2024.09 ~ 현재",
+        description: "인공지능 기반 서비스 개발 동아리 6기 부원",
+        direction: 'left', 
+        delay: 0.4
+    },
+    {
+        title: "SKT FLY AI CHALLENGERS 6기",
+        duration: "2024.12 ~ 2025.02",
+        description: "AI(ML/DL/LLM/OpenCV 등) 부트캠프 수료\nESG 및 창업 기반 Flutter를 통한 AI 어플리케이션 제작",
+        direction: 'right',
+        delay: 0.6
+    }
+];
+
+export default histories;
