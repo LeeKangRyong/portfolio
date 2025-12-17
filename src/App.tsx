e73efb8432a87928f7d8c2dbb8edd99bfb58dafd
LeeKangRyong <gaiogo2@naver.com>
2025-12-17 16:10:04 +0900

    refactor: Migrate to typescript (App, main) #1

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css';
+import { useState } from 'react';
+import TopBar from './components/TopBar/TopBar';
+import AboutMe from './pages/AboutMe/AboutMe';
+import Histories from './pages/History/Histories';
+import Skills from './pages/Skill/Skills';
+import Projects from './pages/Project/Projects';
+import Contact from './pages/Contact/Contact';
+import Skeleton from './components/Skeleton/Skeleton';
+
+function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <Skeleton onComplete={handleLoadingComplete} />;
+  }
+
+  return (
+    <>
+      <TopBar />
+      <main>
+        <section id="A">
+          <AboutMe /> 
+        </section>
+
+        <section id='B'>
+          <Histories />
+        </section>
+
+        <section id='C'>
+            <Skills />
+        </section>
+
+        <section id='D'>  
+          <Projects />        
+        </section>
+        <footer>
+          <Contact />
+        </footer>
+      </main>
+    </>
+  )
+}
+
+export default App;
