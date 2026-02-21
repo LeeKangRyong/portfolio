e601482fd4611a352dbc1d6859e655bd5478e159
LeeKangRyong <gaiogo2@naver.com>
2026-02-21 14:42:36 +0900

    refactor(history): Refactor historylData, no UI change #6

--- src/data/historyData.ts
+++ src/data/historyData.ts
@@ -16,18 +16,32 @@ const histories: History[] = [
     delay: 0.2,
   },
   {
+    title: 'SKT FLY AI CHALLENGERS 6기',
+    duration: '2024.12 ~ 2025.02',
+    description: 'AI(ML/DL/LLM/OpenCV 등) 부트캠프 수료\n창업 기반 프로젝트 수행',
+    direction: 'right',
+    delay: 0.6,
+  },
+  {
     title: '프로메테우스 6기',
-    duration: '2024.09 ~ 현재',
+    duration: '2024.09 ~ 2025.08',
     description: '인공지능 기반 서비스 개발 동아리 6기 부원\n서비스 개발 및 AI 모델 연동 담당',
     direction: 'left',
     delay: 0.4,
   },
   {
-    title: 'SKT FLY AI CHALLENGERS 6기',
-    duration: '2024.12 ~ 2025.02',
-    description: 'AI(ML/DL/LLM/OpenCV 등) 부트캠프 수료',
+    title: '숭실대학교 캡스톤경진대회',
+    duration: '2025.03~2025.05',
+    description: '교내 캡스톤경진대회 중 장려상 수상\nAI 미션 기반 추억 공유 앱서비스 출품',
     direction: 'right',
     delay: 0.6,
+  },
+  {
+    title: 'ZZAN',
+    duration: '2026.01 ~ 현재',
+    description: '실제 앱서비스 운영 중 (FE 담당)\n현) 원스토어 배포 및 운영 중',
+    direction: 'left',
+    delay: 0.8,
   },
 ];
 
